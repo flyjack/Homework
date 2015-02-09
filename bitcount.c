433d777fb895d294ebb62e38279b0105bdd1f4cf
Qingfang Li <jarrodcal@126.com>
2015-02-09 12:24:00 +0800

    Update bitcount.c

--- bitcount.c
+++ bitcount.c
@@ -1,4 +1,5 @@
-//统计一个数中2的个数，1为普通的算法。O(log2V), 2为较优算法，O()只于1的个数有关。
+//2.1 统计二进制数中1的个数
+//get_count1为普通的算法。O(log2V)，即最高位1的位置，get_count2为较优算法，复杂度取决于1的个数。
 
 #include <stdio.h>
 
@@ -35,6 +36,7 @@ int main()
     uint8 a = 65;
 
     get_count1(a);
+    get_count2(a);
 
     return 0;
 }
