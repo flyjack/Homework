fb88ecaba687e1792b812d0663521b4d8cd0b340
Qingfang Li <jarrodcal@126.com>
2015-03-29 11:26:36 +0800

    Update Bitree.c

--- Bitree.c
+++ Bitree.c
@@ -13,11 +13,17 @@ struct Bitree
     int data;
     int index;
     struct Bitree *Lchild,*Rchild;
+    int nMaxLeft;
+    int nMaxRight;
+    struct Bitree *parent;
 };
 
 typedef struct Bitree BitreeNode;
 typedef struct Bitree * LinkBitree;
 
+int nMaxLen = 0;
+
+//树的问题可以转化为链表的问题
 //二叉树的序列化，注意递归，堆栈和队列的使用
 //保存二叉树形态，注意应用i 2i 2i+1的关系
 struct BitreeIndex
@@ -40,7 +46,7 @@ typedef struct Stack StackNode;
 typedef struct Stack * LinkStack;
 
 //建立树的时候保存下标关系
-LinkBitree CreatBitree(int index)
+LinkBitree CreatBitree(int index, LinkBitree parent)
 {
     int data;
     LinkBitree phead;
@@ -60,11 +66,12 @@ LinkBitree CreatBitree(int index)
 
     phead->data = data;
     phead->index = index;
+    phead->parent = parent;
 
     printf("Please Input %d left node \n", phead->data);
-    phead->Lchild = CreatBitree(index*2);
+    phead->Lchild = CreatBitree(index*2, phead);
     printf("Please Input %d right node \n", phead->data);
-    phead->Rchild = CreatBitree(index*2+1);
+    phead->Rchild = CreatBitree(index*2+1, phead);
 
     return phead;
 }
@@ -72,14 +79,14 @@ LinkBitree CreatBitree(int index)
 LinkStack InitStack()
 {
     LinkStack pstack;
-    pstack = (LinkStack)calloc(1, sizeof(StackNode)); 
+    pstack = (LinkStack)calloc(1, sizeof(StackNode));
 
     if (pstack == NULL)
     {
         printf("内存分配失败!");
         exit(0);
     }
-    
+
     return pstack;
 }
 
@@ -90,7 +97,7 @@ void PostorderTraverse(LinkBitree phead, LinkStack pstack1, LinkStack pstack2)
 
 void InorderTraverse(LinkBitree phead, LinkStack pstack)
 {
-    do 
+    do
     {
         while (phead)
         {
@@ -254,7 +261,16 @@ void serializeBitree(LinkBitree phead)
 {
     if (phead)
     {
-        printf("data %d, index %d \n", phead->data, phead->index);
+        if (phead->parent)
+        {
+            LinkBitree parent = phead->parent;
+
+            printf("data %d, index %d, nMaxLeft %d, nMaxRight %d, parent %d \n", phead->data, phead->index, phead->nMaxLeft, phead->nMaxRight, parent->data);
+        }
+        else
+        {
+            printf("data %d, index %d, nMaxLeft %d, nMaxRight %d \n", phead->data, phead->index, phead->nMaxLeft, phead->nMaxRight);
+        }
 
         if (phead->Lchild)
             serializeBitree(phead->Lchild);
@@ -266,10 +282,85 @@ void serializeBitree(LinkBitree phead)
     return;
 }
 
+//转化为求两条链表的交叉点
+LinkBitree findCommonParent(BitreeNode *ch1, BitreeNode *ch2, LinkBitree phead)
+{
+    if (phead == NULL)
+        return NULL;
+
+    if (ch1 == NULL || ch2 == NULL)
+        return NULL;
+
+    BitreeNode *ptree = ch1;
+
+    while (ch2 != phead)
+    {
+        while (ptree != phead)
+        {
+            if (ptree == ch2)
+                return ptree;
+
+            ptree = ptree->parent;
+        }
+
+        ch2 = ch2->parent;
+        ptree = ch1;
+    }
+
+    return phead;
+}
+
+//一个节点的左支点的值是其下面孩子节点值的和且是最大值
+//最大值是全局的变量
+void FindMaxLen(LinkBitree pRoot)
+{
+    if (pRoot == NULL)
+        return;
+
+    if (pRoot -> Lchild == NULL)
+        pRoot -> nMaxLeft = 0;
+
+    if (pRoot -> Rchild == NULL)
+        pRoot -> nMaxRight = 0;
+
+    if (pRoot -> Lchild != NULL)
+        FindMaxLen(pRoot -> Lchild);
+
+    if (pRoot -> Rchild != NULL)
+        FindMaxLen(pRoot -> Rchild);
+
+    if (pRoot -> Lchild != NULL)
+    {
+        int nTempMax = 0;
+
+        if (pRoot->Lchild->nMaxLeft > pRoot->Lchild->nMaxRight)
+            nTempMax = pRoot->Lchild->nMaxLeft;
+        else
+            nTempMax = pRoot->Lchild->nMaxRight;
+
+        pRoot->nMaxLeft = nTempMax + 1;
+    }
+
+    if (pRoot -> Rchild != NULL)
+    {
+        int nTempMax = 0;
+
+        if (pRoot->Rchild->nMaxLeft > pRoot->Rchild->nMaxRight)
+            nTempMax = pRoot->Rchild->nMaxLeft;
+        else
+            nTempMax = pRoot->Rchild->nMaxRight;
+
+        pRoot->nMaxRight = nTempMax + 1;
+    }
+
+    if (pRoot -> nMaxLeft + pRoot -> nMaxRight > nMaxLen)
+        nMaxLen = pRoot -> nMaxLeft + pRoot -> nMaxRight;
+}
+
 int main()
 {
     printf("Please tree node \n");
-    LinkBitree phead = CreatBitree(1);
+    LinkBitree phead = CreatBitree(1, NULL);
     LinkStack pstack = InitStack();
 
     printf("Recursion get tree node:\n");
@@ -280,7 +371,7 @@ int main()
     printf("\n");
     _PostorderTraverse(phead);
     printf("\n");
-    
+
     printf("Non Recursion get tree node \n");
     PreorderTraverse(phead, pstack);
     printf("\n");
@@ -292,10 +383,20 @@ int main()
     leverTraverse(phead, pstack);
 
     printf("---------------------\n");
+    FindMaxLen(phead);
+    printf("Max len is %d\n", nMaxLen);
     serializeBitree(phead);
+
+    printf("---------------------\n");
+    BitreeNode *ptree1 = phead->Lchild->Rchild;
+    BitreeNode *ptree2 = phead->Rchild->Rchild;
+    BitreeNode *tree = findCommonParent(ptree1, ptree2, phead);
+
+    if (tree != NULL)
+        printf("value is %d \n", tree->data);
 
     freeBitree(phead);
     free(pstack);
-    
+
     return 0;
 }
