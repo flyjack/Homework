682c169868b42e3d5c3dc839ae1b57e9b39644be
Qingfang Li <jarrodcal@126.com>
2015-04-09 23:35:48 +0800

    Create ip2int.php

---
+++ ip2int.php
@@ -0,0 +1,22 @@
+<?php
+
+    $ip = "172.16.89.100";
+    $arr = explode(".", $ip);
+
+    $ip1 = intval($arr[0]) << 24;
+    $ip2 = intval($arr[1]) << 16;
+    $ip3 = intval($arr[2]) << 8;
+    $ip4 = intval($arr[3]);
+
+    $v = $ip1 | $ip2 | $ip3 | $ip4;
+    echo $v . "\n";
+
+    $v1 = $v >> 24 & 0xff;
+    $v2 = $v >> 16 & 0xff;
+    $v3 = $v >> 8 & 0xff;
+    $v4 = $v & 0xff;
+
+    echo $v1 . "\n";
+    echo $v2 . "\n";
+    echo $v3 . "\n";
+    echo $v4 . "\n";
